ce3368c59f3505aaa15c7277b51795dcc1112afc
Sebastian <sebastian.thorneus@gmail.com>
2022-05-23 05:26:47 +0100

    fixat designen

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
-
+import {Container, Row, Col,} from "react-bootstrap";
+import { FiDelete } from 'react-icons/fi';
+import { AiFillEdit } from 'react-icons/ai';
+import background from "./images/bakgrund1.jpg"
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" >
+      <Container fluid className="container" >
+  <Row>
+    <Col ><h1 className="title">  [ Todo App ] </h1>  </Col>
+    
+       <input className="form"
+            type="text"
+            name="todo"
+            placeholder=" Enter Todo"
+            tabIndex="1"
+       /> <ul>
+       <li className="todo"><AiFillEdit /> &nbsp;  [ Todo1 ] &nbsp; <FiDelete /></li>
+       <li className="todo"><AiFillEdit /> &nbsp; [ Todo2 ] &nbsp; <FiDelete /></li>
+       <li className="todo"><AiFillEdit /> &nbsp; [ Todo3 ] &nbsp; <FiDelete /></li>
+       </ul>
+  </Row>
+</Container>
     </div>
   );
 }
