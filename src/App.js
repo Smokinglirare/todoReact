d7afa20abe5d0525a084120f7cd8ff8bf1923538
Sebastian Torneus <Sebastian.thorneus@gmail.com>
2022-05-25 11:40:20 +0200

    GET funkar, POST funkar, DELETE funkar, PATCH funkar

--- src/App.js
+++ src/App.js
@@ -1,29 +1,149 @@
 import './App.css';
-import {Container, Row, Col,} from "react-bootstrap";
-import { FiDelete } from 'react-icons/fi';
-import { AiFillEdit } from 'react-icons/ai';
-import background from "./images/bakgrund1.jpg"
+import { Container, Row, Col, } from "react-bootstrap";
+
+import { GiClick } from "react-icons/gi"
+import { MdOutlineDone} from "react-icons/md"
+import {ImCross} from "react-icons/im"
+import React, { useState, useEffect } from "react";
+import axios from 'axios';
+
+
+
 function App() {
+  const [todos, setTodos] = useState([]);
+  const [title, setTitle] = useState("");
+
+
+
+  /* useEffect(() => {
+    fetch('http://localhost:5000/todos')
+    
+    .then(res=>res.json())
+    .then(json=>console.log(json))
+    .then(json =>setTodos(json.todo))
+  
+    
+  }, [])*/
+
+
+  function newTodo() {
+    fetch('http://localhost:5000/todos', {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(
+        {
+          todo: title
+        }
+      )
+    })
+    .then(() => {
+      getTodos();
+    })
+
+  }
+  function handleSubmit(e) {
+    e.preventDefault();
+    newTodo(title);
+
+  }
+
+  /*      function submitTodo() {
+          newTodo(setTitle);
+        } */
+  /*       fetch(`http://localhost:5000/todos/${id}`,{
+           method:"PUT",
+           body:JSON.stringify(
+               {
+                   todo: "Böla"
+               }
+           )
+       })
+           .then(res=>res.json())
+           .then(json=>console.log(json))
+*/
+
+  function handleUpdate(id) {
+    fetch(`http://localhost:5000/todos/${id}`, {
+      method: "PATCH",
+      body: JSON.stringify(
+        {
+          completed: true
+        }
+      )
+    })
+      .then(() => {
+        getTodos();
+      })
+  }
+
+
+
+  function handleDelete(id) {
+    fetch(`http://localhost:5000/todos/${id}`, {
+      method: "DELETE"
+    })
+      .then(() => {
+        getTodos();
+      })
+  }
+
+
+
+
+
+  function getTodos() {
+    axios
+      .get("http://localhost:5000/todos")
+      .then((response) => {
+
+        setTodos(response.data);
+        
+      })
+      .catch((error) => console.log(error));
+    console.log("data laddades in")
+
+
+  }
+
+  useEffect(() => {
+    getTodos();
+  }, []);
+
+
   return (
+
+
     <div className="App" >
       <Container fluid className="container" >
-  <Row>
-    <Col ><h1 className="title">  [ Todo App ] </h1>  </Col>
-    
-       <input className="form"
-            type="text"
-            name="todo"
-            placeholder=" Enter Todo"
-            tabIndex="1"
-       /> <ul>
-       <li className="todo"><AiFillEdit /> &nbsp;  [ Todo1 ] &nbsp; <FiDelete /></li>
-       <li className="todo"><AiFillEdit /> &nbsp; [ Todo2 ] &nbsp; <FiDelete /></li>
-       <li className="todo"><AiFillEdit /> &nbsp; [ Todo3 ] &nbsp; <FiDelete /></li>
-       </ul>
-  </Row>
-</Container>
+        <Row>
+          <Col ><h1 className="title">  [ Todo App ] </h1>  </Col>
+          <form onSubmit={handleSubmit}>
+            <input className="form"
+              type="text"
+              name="todo"
+              placeholder=" Enter Todo"
+              tabIndex="1"
+              value={title}
+              onChange={(e) => setTitle(e.target.value)}
+            />
+            <button type="submit" className='todo-button'
+            >
+              <GiClick className="click-picture" />
+            </button>
+          </form>
+          <ul className="todos">
+            {todos.map(todo => (
+              <li key={todo.id}><button className={todo.completed ? "done" : ""} onClick={() => handleUpdate(todo.id)}><MdOutlineDone /></button> {todo.todo} <button onClick={() => handleDelete(todo.id)}><ImCross /></button></li>
+            ))}
+          </ul>
+
+        </Row>
+      </Container>
     </div>
   );
 }
 
 export default App;
+
